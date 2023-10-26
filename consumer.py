6e49e1c9eca13c732333b487d4fb21086793a5d1
Duncan.Pham <duncan.pham@vincere.io>
2023-10-26 18:03:59 +0700

    first commit

---
+++ consumer.py
@@ -0,0 +1,102 @@
+from kafka import KafkaConsumer, TopicPartition
+from json import loads
+import io
+import pandas as pd
+from producer import get_database_engine
+from config import config
+import gzip
+import logging
+
+logging.basicConfig(level=logging.INFO,
+					format='%(asctime)s:%(funcName)s:%(levelname)s:%(message)s')
+
+
+def get_file(id: str):
+	"""
+    Retrieves a file from the database using the given attachment ID and saves it locally.
+    
+    Args:
+        id (str): Attachment ID used to fetch the file from the database.
+        
+    Raises:
+        gzip.BadGzipFile: If the extracted file is not a valid GZIP file.
+    """
+	db_engine = get_database_engine(config)
+	sql = f"""
+		SELECT vchAttachmentName, convert(varbinary(max), binDocbytes, 1) as binDocBytes
+		FROM dAttachmentBinaryDocuments ab
+		join dAttachment a on ab.intAttachmentId = a.intAttachmentId
+		where ab.intAttachmentId = {id}"""
+	df = pd.read_sql(sql, db_engine)
+	dest_path = 'D:\\extract_docs_kafka\\file\\'
+	data = df.values
+	file_name = data[0][0]
+	varbinary = data[0][1]
+	file_path = dest_path + file_name
+	try:
+		with gzip.GzipFile(fileobj=io.BytesIO(varbinary), mode='rb') as f_in:
+			with open(file_path, 'wb') as f_out:
+				f_out.write(f_in.read())
+	except gzip.BadGzipFile as err:
+		logging.warn(f"{err}{file_name} can't be extract")
+
+
+def save_offset(offset: int):
+	"""
+    Saves the provided offset to a file for resuming Kafka message consumption.
+    
+    Args:
+        offset (int): The offset value to be saved.
+    """
+	with open(offset_file, 'w') as f:
+		f.write(str(offset))
+
+
+def read_offset():
+	"""
+    Reads the last saved offset from the file.
+    
+    Returns:
+        int or None: The last saved offset if the file exists, else None.
+    """
+	try:
+		with open(offset_file, 'r') as f:
+			return int(f.read())
+	except FileNotFoundError:
+		return None
+
+
+def consume_data(topic: str, bootstrap_servers: list[str]):
+	"""
+    Consumes messages from the specified Kafka topic starting from the last saved offset.
+    Processes each message by fetching the corresponding file from the database.
+    
+    Args:
+        topic (str): The Kafka topic to consume messages from.
+        bootstrap_servers (list[str]): List of Kafka broker addresses.
+    """
+	logging.info('Start consume data...')
+	current_offset = read_offset()
+	if current_offset is None:
+		current_offset = 0
+	consumer = KafkaConsumer(bootstrap_servers=bootstrap_servers,
+							 value_deserializer=lambda x: loads(x.decode('utf-8')))
+
+	partition = TopicPartition(topic, 0)
+	consumer.assign([partition])
+	consumer.seek(partition=partition, offset=current_offset)
+
+	for message in consumer:
+		offset = message.offset
+		id = message.value
+		print(f"Offset: {offset}, Value: {id}")
+		get_file(id)
+		save_offset(offset)
+
+	consumer.close()
+
+
+if __name__ == '__main__':
+	offset_file = 'offset.txt'
+	
+	consume_data('extract_data', ['localhost:9092'])
---
+++ producer.py
@@ -0,0 +1,85 @@
+from kafka import KafkaProducer
+import json
+from config import config
+import sqlalchemy
+import pandas as pd
+import time
+import logging
+
+logging.basicConfig(level=logging.INFO,
+					format='%(asctime)s:%(funcName)s:%(levelname)s:%(message)s')
+
+
+def get_database_engine(config: dict):
+	"""
+    Establishes a connection to the database based on the provided configuration.
+    
+    Args:
+        config (dict): Dictionary containing database connection details.
+        
+    Returns:
+        sqlalchemy.engine.Engine: Database engine object for executing SQL queries.
+    """
+	if config['system'] == 'mssql':
+		engine = sqlalchemy.create_engine(
+			'mssql+pymssql://{0}:{1}@{2}:{3}/{4}?charset=utf8'.format(
+				config['user'], config['password'],
+				config['server'], config['port'],
+				config['database']))
+
+	if config['system'] == 'mysql':
+		engine = sqlalchemy.create_engine(
+			'mysql+pymysql://{0}:{1}@{2}/{3}'.format(
+				config['user'], config['password'],
+				config['server'], config['database']))
+
+	if config['system'] == 'postgresql':
+		engine = sqlalchemy.create_engine(
+			'postgresql+psycopg2://{0}:{1}@{2}:{3}/{4}'.format(
+				config['user'], config['password'],
+				config['server'], config['port'],
+				config['database']),
+			pool_size=100,
+			max_overflow=200,
+			client_encoding='utf8')
+	return engine
+
+
+def get_id() -> list[int | str]:
+	"""
+    Retrieves a list of document IDs from the database.
+    
+    Returns:
+        list[int | str]: List of document IDs.
+    """
+	db_engine = get_database_engine(config)
+	sql = """select top 100 intAttachmentId from dAttachmentBinaryDocuments"""
+	df = pd.read_sql(sql, db_engine)
+	col_name = df.columns[0]
+	ids = df[col_name].to_list()
+	return ids
+
+
+def produce_data_to_kafka(document_ids: list[int], topic, bootstrap_servers: list[str]):
+	"""
+    Produces document IDs to a Kafka topic.
+
+    Args:
+        document_ids (list[int]): List of document IDs to be sent to Kafka.
+        topic (str): The Kafka topic to which the messages will be sent.
+        bootstrap_servers (list[str]): List of Kafka broker addresses.
+    """
+	logging.info('Start produce data...')
+	producer = KafkaProducer(bootstrap_servers=bootstrap_servers)
+	for id in document_ids:
+		producer.send(topic, json.dumps(str(id)).encode('utf-8'))
+		producer.flush()
+		time.sleep(2)
+
+
+if __name__ == '__main__':
+	ids = get_id()
+	produce_data_to_kafka(ids, 'extract_data', ['localhost:9092'])
+
+# kafka-server-start.bat D:\Apps\Kafka\config\server.properties
+# zookeeper-server-start.bat D:\Apps\Kafka\config\zookeeper.properties
